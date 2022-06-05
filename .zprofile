dbc715655b0b5ecc7cae3cc8ddb1fe1973d3e69a
ent <ustcglt@mail.ustc.edu.cn>
2022-06-05 10:37:09 +0800

    代理改为proxychains与trojan

--- .zprofile
+++ .zprofile
@@ -1,11 +1,1 @@
 xmodmap -e "clear Lock" -e "keycode 66 = Escape"
-function proxy_on() {
-    export http_proxy=http://127.0.0.1:12333
-    export https_proxy=
-    echo -e "终端代理已开启。"
-}
-
-function proxy_off(){
-    unset http_proxy https_proxy
-    echo -e "终端代理已关闭。"
-}
--- personal_conf.sh
+++ personal_conf.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-sudo apt install zsh tmux git wget xsel
+sudo apt install zsh tmux git wget xsel proxychains
+sudo chmod 777 /etc/proxychains.conf
+sudo sed -i '/socks4  127.0.0.1/d' /etc/proxychains.conf
+sudo sed -i '/socks5  127.0.0.1/d' /etc/proxychains.conf
+echo "socks5  127.0.0.1 51837" >> /etc/proxychains.conf
 # 安装oh my zsh
 wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -
 # 安装 oh my zsh 主题需要的字体
